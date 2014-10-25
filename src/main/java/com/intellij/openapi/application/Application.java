a2fddb07a85258ae36be41fd12e8bc6b0c49e32a
oleg <oleg@legatus.ru>
2014-10-25 16:24:36 +0400

    Adding options and saveToZip feature

--- src/main/java/com/intellij/openapi/application/Application.java
+++ src/main/java/com/intellij/openapi/application/Application.java
@@ -5,6 +5,6 @@ import jd.ide.intellij.config.JDPluginComponent;
 
 public class Application implements ComponentManager {
     public <T> T getComponent(Class<T> interfaceClass) {
-        return (T)(new JDPluginComponent());
+        return (T)(JDPluginComponent.CONF);
     }
 }
--- src/main/java/jd/core/Decompiler.java
+++ src/main/java/jd/core/Decompiler.java
@@ -1,14 +1,15 @@
 package jd.core;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.zip.ZipOutputStream;
+
 import jd.ide.intellij.JavaDecompiler;
+import jd.ide.intellij.config.JDPluginComponent;
 
 public class Decompiler {
     private JavaDecompiler decompiler;
@@ -24,12 +25,56 @@ public class Decompiler {
         return decompiled;
     }
 
+    public int decompileToDir(String jarPath, String outDir) throws DecompilerException, IOException {
+        String name = jarPath.substring(0, jarPath.lastIndexOf(".jar"));
+
+        Map<String, String> pathToSrc = decompile(jarPath);
+
+        if(JDPluginComponent.CONF.isSaveToZip()) {
+            return decompileToZip(name, pathToSrc);
+        } else {
+            return decompileToDir(outDir == null ? name : outDir, pathToSrc);
+        }
+    }
+
+    private int decompileToDir(String outDir, Map<String, String> pathToSrc) throws FileNotFoundException {
+        for (Map.Entry<String, String> entry : pathToSrc.entrySet()) {
+            String fileName = entry.getKey();
+            File file = new File(outDir, fileName);
+            file.getParentFile().mkdirs();
+            PrintWriter out = new PrintWriter(file);
+            out.print(entry.getValue());
+            out.close();
+        }
+        return pathToSrc.size();
+    }
+
+    public int decompileToZip(String name, Map<String, String> entries) throws DecompilerException, IOException {
+        String zipName = name + ".src.zip";
+
+        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipName));
+        PrintWriter printOut = new PrintWriter(zipOut);
+
+        for(Map.Entry<String, String> entry : entries.entrySet()) {
+            zipOut.putNextEntry(new ZipEntry(entry.getKey()));
+            printOut.print(entry.getValue());
+            printOut.flush();
+            zipOut.closeEntry();
+        }
+
+        zipOut.finish();
+        zipOut.close();
+
+        return entries.size();
+    }
+
     public Map<String, String> decompile(String jarPath) throws DecompilerException, IOException {
         ZipInputStream zip = new ZipInputStream(new FileInputStream(jarPath));
         ZipEntry ze;
         Map<String, String> pathToSrc = new HashMap<String, String>();
         CaseInsensitiveFilePathSet caseInsensitiveSet = new CaseInsensitiveFilePathSet();
 
+        Scanner in = new Scanner(zip);
         while ((ze = zip.getNextEntry()) != null ) {
             String entryName = ze.getName();
             if (entryName.endsWith(".class")) {
@@ -46,23 +91,16 @@ public class Decompiler {
                     caseInsensitiveSet.add(javaPath);
                     pathToSrc.put(javaPath, decompiler.decompile(jarPath, classPath));
                 }
+            } else if( !ze.isDirectory() ) {
+                StringBuilder entry = new StringBuilder();
+                while(in.hasNextLine()) {
+                    entry.append(in.nextLine()).append('\n');
+                }
+                pathToSrc.put(ze.getName(), entry.toString());
             }
         }
 
         return pathToSrc;
-    }
-
-    public int decompileToDir(String jarPath, String outDir) throws DecompilerException, IOException {
-        Map<String, String> pathToSrc = decompile(jarPath);
-        for (Map.Entry<String, String> entry : pathToSrc.entrySet()) {
-            String fileName = entry.getKey();
-            File file = new File(outDir, fileName);
-            file.getParentFile().mkdirs();
-            PrintWriter out = new PrintWriter(file);
-            out.print(entry.getValue());
-            out.close();
-        }
-        return pathToSrc.size();
     }
 
     private boolean validContent(String decompiled) {
